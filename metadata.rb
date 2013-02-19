f0409d7c922cb7ae6605a9a09ebf711ab6baa361
Seth Chisamore <schisamo@opscode.com>
2013-02-18 20:22:04 -0500

    add supports fields to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      "Deploys and manages an instance of Github's Hubot."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
+supports "ubuntu", ">= 10.04"
+supports "centos", ">= 6.0"
+
 depends "git"
 depends "nodejs"
 depends "runit"
