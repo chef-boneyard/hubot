baa92776b188b05f169e8e3d320b483d2f3dd6d9
Tim Smith <tim@cozy.co>
2015-02-23 16:54:04 -0800

    Update metadata contacts and add RHEL support

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'hubot'
-maintainer       'Seth Chisamore'
-maintainer_email 'schisamo@opscode.com'
+maintainer       'Tim Smith'
+maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      "Deploys and manages an instance of Github's Hubot."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,6 +8,7 @@ version          '1.0.3'
 
 supports 'ubuntu', '>= 10.04'
 supports 'centos', '>= 6.0'
+supports 'redhat', '>= 6.0'
 
 depends 'git'
 depends 'nodejs'
