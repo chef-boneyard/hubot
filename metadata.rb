686643dbb865faff7335abb6ce367e8453efb92c
Barthelemy Vessemont <bvessemont@gmail.com>
2015-04-24 22:03:35 +0200

    rollback cookbook version to upstream HEAD

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "schisamo@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and manages an instance of Github's Hubot."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          "1.0.5"
 
 supports "ubuntu", ">= 10.04"
 supports "centos", ">= 6.0"
