754719c01dd5574d70c998bf7cd26ab180bf52b4
Tim Smith <tsmith@chef.io>
2016-12-02 15:03:44 -0800

    Use apt_update resource for testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,4 +1,8 @@
+apt_update 'update' if platform_family?('debian')
+
 include_recipe 'git::default'
+
+# make sure node is actually installed
 include_recipe 'nodejs::default'
 
 hubot_instance 'bug_bot'
