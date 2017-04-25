6b701e0eaee0eb4030782ed937b599d65c3a6f52
Tim Smith <tsmith@chef.io>
2017-04-24 23:33:01 -0700

    Use SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'hubot'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      "Deploys and manages an instance of Github's Hubot."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.7'
